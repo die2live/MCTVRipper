9d927519d073516e2db42d78843c8b469fb6ce89
die2live <jcswaiting4u@gmail.com>
2014-03-20 21:38:15 +0200

    get category

--- Article.py
+++ Article.py
@@ -1,5 +1,7 @@
 __author__ = 'DAi'
 
+import re
+
 
 class Article:
     title = ""
@@ -12,11 +14,18 @@ class Article:
         #TODO: implement title constructor
         return title
 
+    def __get_category(self, title):
+        #TODO: implement title constructor
+        m = re.search(r'http://www.youtube.com/embed/(.*)(\" )+', title)
+
+        return title
+
     def __init__(self, title, link, description, feed_alias):
         self.title = self.__get_title(title)
         self.link = link
         self.description = description
         self.feed_alias = feed_alias
+        self.category = self.__get_category(title)
 
     def __str__(self):
         return self.title
